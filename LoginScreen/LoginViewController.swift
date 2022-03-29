0b799c170dc099909911f005e9eab14b7e0c981a
jojoch <vzelinkova@email.cz>
2022-03-29 22:01:19 +0200

    Polish code

--- LoginScreen/LoginViewController.swift
+++ LoginScreen/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  LoginScreen
 //
 //  Created by Veronika Zelinkova on 29.03.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
