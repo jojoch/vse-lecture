a88aaa6bf9361827a158b6a9ebcb7ddd8ff0b0ea
jojoch <vzelinkova@email.cz>
2022-03-29 11:12:24 +0200

    Polish code

--- LoginScreen/Styles/Color+App.swift
+++ LoginScreen/Styles/Color+App.swift
@@ -5,32 +5,11 @@
 //  Created by Jan Kaltoun on 29.01.2022.
 //
 
-import SwiftUI
+import UIKit
 
-// MARK: - SwiftUI
-extension Color {
-    static let appBackgroundGradientTop = Color("Colors/Background/Gradient/Top")
-    static let appBackgroundGradientBottom = Color("Colors/Background/Gradient/Bottom")
-    static let appBackgroundItem = Color("Colors/Background/Item")
-    
-    static let appTextBody = Color("Colors/Text/Body")
-    static let appTextSectionTitle = Color("Colors/Text/Section Title")
-    static let appTextItemTitle = Color(uiColor: .appTextItemTitle)
-    static let appTextNavigationBar = Color(uiColor: .appTextNavigationBar)
-}
-
-// MARK: - UIKit
 extension UIColor {
-    static let appBackgroundTabBar = UIColor(named: "Colors/Background/Tab Bar")!
-    static let appBackgroundSegmentedControlGeneral = UIColor(named: "Colors/Background/Segmented Control/General")!
-    static let appBackgroundSegmentedControlSelected = UIColor(named: "Colors/Background/Segmented Control/Selected")!
-    
-    static let appTextNavigationBar = UIColor(named: "Colors/Text/Navigation Bar")!
-    
-    static let appTintNavigationBar = UIColor(named: "Colors/Tint/Navigation Bar")!
-    static let appTintTabBarSelected = UIColor(named: "Colors/Tint/Tab Bar/Selected")!
-    static let appTintTabBarUnselected = UIColor(named: "Colors/Tint/Tab Bar/Unselected")!
-    
-    static let appTextItemTitle = UIColor(named: "Colors/Text/Item Title")!
-    static let appTextItemTitleInverse = UIColor(named: "Colors/Text/Item Title Inverse")!
+    static let appBackground = UIColor(named: "Colors/background")
+    static let appText = UIColor(named: "Colors/text")
+    static let appButtonBackground = UIColor(named: "Colors/buttonBackground")
+    static let appAction = UIColor(named: "Colors/action")
 }
