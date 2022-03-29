14cd20648703e3a6c8501cf3cd05af65d55db4c2
jojoch <vzelinkova@email.cz>
2022-03-30 00:27:33 +0200

    Add constants

--- LoginScreen/LoginViewController.swift
+++ LoginScreen/LoginViewController.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    // MARK: - Constants
+    private let doneButtonBottomPaddingKeyboardUp: CGFloat = 15
+    private let doneButtonBottomPaddingKeyboardDown: CGFloat = 47
+    
+    // MARK: - Private Properties
+    /*private var isEmailValid: Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        return email.range(of: emailRegEx, options: .regularExpression, range: nil, locale: nil) != nil
+    }*/
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
